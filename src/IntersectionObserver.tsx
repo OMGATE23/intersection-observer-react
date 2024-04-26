9c435edb10bcf3370923761528e87c79423758f1
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-04-26 11:13:33 +0530

    chore : beautify code
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

--- src/IntersectionObserver.tsx
+++ src/IntersectionObserver.tsx
@@ -1,31 +1,40 @@
-import React , {ReactNode , useEffect, useRef} from 'react'
+import React, { ReactNode, useEffect, useRef } from "react";
 
-type IntersectionObserverCallback = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => void;
-
+type IntersectionObserverCallback = (
+  entries: IntersectionObserverEntry[],
+  observer: IntersectionObserver
+) => void;
 
 type Props = {
-    onEnter ?: IntersectionObserverCallback,
-    onLeave ?: IntersectionObserverCallback,
-    options ?: IntersectionObserverInit,
-    once ?: boolean 
-}
+  onEnter?: IntersectionObserverCallback;
+  onLeave?: IntersectionObserverCallback;
+  options?: IntersectionObserverInit;
+};
 export default function Observer({
-    onEnter = () => {} , onLeave = () => {} , options
-} : Props){
-    const ref = useRef<HTMLDivElement | null>(null)
+  onEnter = () => {},
+  onLeave = () => {},
+  options,
+}: Props) {
+  const ref = useRef<HTMLDivElement | null>(null);
 
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries , observer) =>{
-            let [observedComponent] = entries
-            if(observedComponent.isIntersecting){
-                onEnter(entries , observer)
-            } else {
-                onLeave(entries , observer)
-            }
-        } , options)
-        if(ref.current){
-            observer.observe(ref.current)
-        }
-    } , [])
-    return <div ref = {ref} style={{height : "1px"}}></div>
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries, observer) => {
+      let [observedComponent] = entries;
+      if (observedComponent.isIntersecting) {
+        onEnter(entries, observer);
+      } else {
+        onLeave(entries, observer);
+      }
+    }, options);
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+  return <div ref={ref} style={{ height: "1px" }}></div>;
 }
